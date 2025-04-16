ffd1fb043c1307e9353436b00bcc46c0a1811d31
Lencana <lensa918@gmail.com>
2025-04-16 18:47:08 +0800

    Add files via upload

---
+++ Meet1.py
@@ -0,0 +1,39 @@
+# perbanfingan kemungkinan nilai = true/salah (bolean)
+# 1 > 2 =
+#<,>,>=,<=,!=,not
+
+#<
+a = 24
+b = 89
+hasil = a < b
+print(hasil)
+
+#>
+a = 24
+b = 89
+hasil = a > b
+print(hasil)
+
+#>=
+a = 24
+b = 89
+hasil = a >= b
+print(hasil)
+
+#<=
+a = 24
+b = 89
+hasil = a <= b
+print(hasil)
+
+#!=
+a = 24
+b = 89
+hasil = a != b
+print(hasil)
+
+#not
+a = 24
+b = 89
+hasil = a not b
+print(hasil)
