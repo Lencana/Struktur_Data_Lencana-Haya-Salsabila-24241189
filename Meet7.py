14b42e8890fa0d9867d82d743153fbc53efd107c
Lencana <lensa918@gmail.com>
2025-06-13 09:52:40 +0800

    Add files via upload

---
+++ Meet7.py
@@ -0,0 +1,10 @@
+# tanggal_lahir = input("Masukkan tempat lahir anda")
+# hasil = len(tanggal_lahir)
+# print(hasil)
+
+password = input("Masukkan password anda: ")
+
+if len(password) < 8:
+    print("Karakter kurang")
+else:
+    print("Karakter cukup")
